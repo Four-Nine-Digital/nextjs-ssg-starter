e25521b4e91a70a2aa75e369e89f619b9e0e6d6a
Andrew Han <andrew@techsamurais.com>
2023-12-26 16:10:52 -0800

    add: deploy action yml, next config output export, and src for Image element

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/ssg-next-github-page-starter',
+  images: {
+    unoptimized: true
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
