c38655952c00266dfa8995ee3f54cb95b23fe1fc
Andrew Han <andrew@techsamurais.com>
2024-01-03 14:41:46 -0800

    add: styles for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,16 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { fetchPage } from '../api/fetch';
 
 export default function Home({ content }) {
-  console.log({ content });
   const { title, body } = content.fields;
 
   return (
-    <main>
+    <main className="prose-h1:text-4xl prose-h2:text-3xl prose-p:text-xl space-y-8 m-6">
       <h1>{title}</h1>
       {body.map(({ fields: { title, text }, sys }) => {
         return (
-          <div key={sys.id}>
-            <h1>{title}</h1>
-            {documentToReactComponents(text)}
+          <div key={sys.id} className="space-y-4">
+            <h2>{title}</h2>
+            <div className="space-y-3">{documentToReactComponents(text)}</div>
           </div>
         );
       })}
